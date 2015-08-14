f82f508ee6593d21d58aeeca08636e701b981522
Josh Harris <tateu@tateu.net>
2015-08-14 10:12:00 -0700

    Consistent Formatting. Leading spaces to tabs, trim EOL blank space.

--- main.m
+++ main.m
@@ -38,7 +38,7 @@ static void setBrightness(float br, bool skipDeltaCheck)
 	{
 		int port = useBackBoardServices ? 1 : SBSSpringBoardServerPort();
 		int numSteps = diff/0.002f; // how big a step will be
-		if (numSteps == 0) 
+		if (numSteps == 0)
 			numSteps = 1;
 		else if (numSteps > 20)
 			numSteps = 20;
@@ -79,17 +79,17 @@ void handle_event1 (void* target, void* refcon, IOHIDEventQueueRef queue, IOHIDE
 {
 	if (s_screenBlanketed) return;
 
-    if (IOHIDEventGetType(event)==kIOHIDEventTypeAmbientLightSensor)
-    { 
-        int luxNow = IOHIDEventGetIntegerValue(event, (IOHIDEventField)kIOHIDEventFieldAmbientLightSensorLevel); // lux Event Field
-        static int luxTotal = 0;
+	if (IOHIDEventGetType(event)==kIOHIDEventTypeAmbientLightSensor)
+	{
+		int luxNow = IOHIDEventGetIntegerValue(event, (IOHIDEventField)kIOHIDEventFieldAmbientLightSensorLevel); // lux Event Field
+		static int luxTotal = 0;
 		static int luxNum = 0;
-		
+
 		// comput time elapset...
 		static time_t lastTime = 0;
 		time_t tim = time(NULL);
-		if (lastTime == 0 || s_resetLux) 
-		{ 
+		if (lastTime == 0 || s_resetLux)
+		{
 			// first time this function is called - set to "now" values
 			lastTime = tim - 10;
 			luxTotal = luxNow;
@@ -99,43 +99,43 @@ void handle_event1 (void* target, void* refcon, IOHIDEventQueueRef queue, IOHIDE
 		NSLog(@"AutoBrightness: %d got new data - lux now %d", tim, luxNow);
 #endif
 
-		// skip if less than x-seconds since the last event 
-		if (tim - lastTime < 4 && !s_resetLux) 
+		// skip if less than x-seconds since the last event
+		if (tim - lastTime < 4 && !s_resetLux)
 		{
 			luxTotal += luxNow;
 			luxNum++;
 			return;
 		}
-		lastTime = tim;	
+		lastTime = tim;
 		s_resetLux = false;
 
 		// compute filtered lux
 		int luxValue = ((float)luxTotal)/luxNum;
-		luxTotal = luxNow; 
-		luxNum = 1; 
-        
-        // comute current brightness from lux value
-        //float br = sqrtf(luxValue)/50.0f;
+		luxTotal = luxNow;
+		luxNum = 1;
+
+		// comute current brightness from lux value
+		//float br = sqrtf(luxValue)/50.0f;
 		float br = 0.199311f*logf(0.03f*luxValue+1.0f);
-        if (br>1) br = 1;
+		if (br>1) br = 1;
 		//NSLog(@"LUX: %d -> %.4f", luxValue, br);
-        
-        // get old brightness
-        //static float oldBr = 0;
-        //if (oldBr == 0) oldBr = br;
-        
-        // smooth brightness
-        //br = (oldBr*2+br)/3.0f;
-        
-        // set
+
+		// get old brightness
+		//static float oldBr = 0;
+		//if (oldBr == 0) oldBr = br;
+
+		// smooth brightness
+		//br = (oldBr*2+br)/3.0f;
+
+		// set
 #ifdef AB_LOG
 		NSLog(@"AutoBrightness: %d timeout %d - computed %f brightness", tim, tim - lastTime, br);
 #endif
 		setBrightness(br, s_resetLux);
-        
-        // store...
-        //oldBr = br;
-    }
+
+		// store...
+		//oldBr = br;
+	}
 }
 
 void reloadPrefs()
@@ -145,27 +145,27 @@ void reloadPrefs()
 #endif
 
 	NSDictionary* prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/me.k3a.ab.plist"];
-    bool enabled = true; // enabled by default
-    
+	bool enabled = true; // enabled by default
+
 	if (prefs)
-    {
-        enabled = [[prefs objectForKey:@"enabled"] boolValue];
-    }
-    
-    if (!s_running && enabled) // enable!
-    {
-        IOHIDEventSystemClientScheduleWithRunLoop(s_hidSysC, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
-        IOHIDEventSystemClientRegisterEventCallback(s_hidSysC, handle_event1, NULL, NULL);
-        s_running = true;
-        NSLog(@"AB: enabled");
-    }
-    else if (s_running && !enabled) // disable!
-    {
-        IOHIDEventSystemClientUnscheduleWithRunLoop(s_hidSysC, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
-        IOHIDEventSystemClientUnregisterEventCallback(s_hidSysC);
-        s_running = false;
-        NSLog(@"AB: disabled");
-    }
+	{
+		enabled = [[prefs objectForKey:@"enabled"] boolValue];
+	}
+
+	if (!s_running && enabled) // enable!
+	{
+		IOHIDEventSystemClientScheduleWithRunLoop(s_hidSysC, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+		IOHIDEventSystemClientRegisterEventCallback(s_hidSysC, handle_event1, NULL, NULL);
+		s_running = true;
+		NSLog(@"AB: enabled");
+	}
+	else if (s_running && !enabled) // disable!
+	{
+		IOHIDEventSystemClientUnscheduleWithRunLoop(s_hidSysC, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+		IOHIDEventSystemClientUnregisterEventCallback(s_hidSysC);
+		s_running = false;
+		NSLog(@"AB: disabled");
+	}
 }
 
 int main()
@@ -218,45 +218,45 @@ int main()
 		}
 	}
 
-    // ------- get ALS service -----------
-    
-    int pv1 = 0xff00;
-    int pv2 = 4;
-    CFNumberRef mVals[2];
-    CFStringRef mKeys[2];
-    
-    mVals[0] = CFNumberCreate(CFAllocatorGetDefault(), kCFNumberSInt32Type, &pv1);
-    mVals[1] = CFNumberCreate(CFAllocatorGetDefault(), kCFNumberSInt32Type, &pv2);
-    mKeys[0] = CFStringCreateWithCString(0, "PrimaryUsagePage", 0);
-    mKeys[1] = CFStringCreateWithCString(0, "PrimaryUsage", 0);
-    
-    CFDictionaryRef matchInfo = CFDictionaryCreate(CFAllocatorGetDefault(),(const void**)mKeys,(const void**)mVals, 2, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-    
-    s_hidSysC = IOHIDEventSystemClientCreate(kCFAllocatorDefault);
-    IOHIDEventSystemClientSetMatching(s_hidSysC,matchInfo);
-    
-    CFArrayRef matchingsrvs = IOHIDEventSystemClientCopyServices(s_hidSysC,0);
-    
-    if (CFArrayGetCount(matchingsrvs) == 0)
-    {
-        NSLog(@"AutoBrightness: ALS Not found!");
-        return 1;
-    }
-    
-    // ----- configure the service -----------------
-    
-    IOHIDServiceClientRef alssc = (IOHIDServiceClientRef)CFArrayGetValueAtIndex(matchingsrvs, 0);
+	// ------- get ALS service -----------
 
-    int ri = 500000;//1000;
-    CFNumberRef interval = CFNumberCreate(CFAllocatorGetDefault(), kCFNumberIntType, &ri);
-    IOHIDServiceClientSetProperty(alssc,CFSTR("ReportInterval"),interval);
-    
-    // ----- set ALS callback -----------------
-    
+	int pv1 = 0xff00;
+	int pv2 = 4;
+	CFNumberRef mVals[2];
+	CFStringRef mKeys[2];
+
+	mVals[0] = CFNumberCreate(CFAllocatorGetDefault(), kCFNumberSInt32Type, &pv1);
+	mVals[1] = CFNumberCreate(CFAllocatorGetDefault(), kCFNumberSInt32Type, &pv2);
+	mKeys[0] = CFStringCreateWithCString(0, "PrimaryUsagePage", 0);
+	mKeys[1] = CFStringCreateWithCString(0, "PrimaryUsage", 0);
+
+	CFDictionaryRef matchInfo = CFDictionaryCreate(CFAllocatorGetDefault(),(const void**)mKeys,(const void**)mVals, 2, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+
+	s_hidSysC = IOHIDEventSystemClientCreate(kCFAllocatorDefault);
+	IOHIDEventSystemClientSetMatching(s_hidSysC,matchInfo);
+
+	CFArrayRef matchingsrvs = IOHIDEventSystemClientCopyServices(s_hidSysC,0);
+
+	if (CFArrayGetCount(matchingsrvs) == 0)
+	{
+		NSLog(@"AutoBrightness: ALS Not found!");
+		return 1;
+	}
+
+	// ----- configure the service -----------------
+
+	IOHIDServiceClientRef alssc = (IOHIDServiceClientRef)CFArrayGetValueAtIndex(matchingsrvs, 0);
+
+	int ri = 500000;//1000;
+	CFNumberRef interval = CFNumberCreate(CFAllocatorGetDefault(), kCFNumberIntType, &ri);
+	IOHIDServiceClientSetProperty(alssc,CFSTR("ReportInterval"),interval);
+
+	// ----- set ALS callback -----------------
+
 	// will be set later in reloadPrefs
-    /*IOHIDEventSystemClientScheduleWithRunLoop(s_hidSysC, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
-    IOHIDEventSystemClientRegisterEventCallback(s_hidSysC, handle_event1, NULL, NULL);
-    s_running = true;*/
+	/*IOHIDEventSystemClientScheduleWithRunLoop(s_hidSysC, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+	IOHIDEventSystemClientRegisterEventCallback(s_hidSysC, handle_event1, NULL, NULL);
+	s_running = true;*/
 
 	// ----------- set lock notif ---------------------
 	int notifyToken;
@@ -265,11 +265,11 @@ int main()
 										  dispatch_get_main_queue(), ^(int t) {
 											  uint64_t state;
 											  int result = notify_get_state(t, &state);
-		s_screenBlanketed = state != 0;
-											  if (!s_screenBlanketed) 
-											  { 
-											  	setBrightness(s_lastBr, true);
-											  	s_resetLux = true;
+											  s_screenBlanketed = state != 0;
+											  if (!s_screenBlanketed)
+											  {
+												  setBrightness(s_lastBr, true);
+												  s_resetLux = true;
 											  }
 											  //NSLog(@"AutoBrightness: lock state change = %llu", state);
 											  if (result != NOTIFY_STATUS_OK) {
@@ -287,15 +287,15 @@ int main()
 										dispatch_get_main_queue(), ^(int t) {
 											reloadPrefs();
 										});
-    
-    if (status != NOTIFY_STATUS_OK) {
+
+	if (status != NOTIFY_STATUS_OK) {
 		NSLog(@"AutoBrightness: toggle notif: notify_register_dispatch() not returning NOTIFY_STATUS_OK");
 	}
 
 	// -------- run! ------------------------------
 	reloadPrefs();
-    CFRunLoopRun();
+	CFRunLoopRun();
 
 	return 0;
-    
+
 }
